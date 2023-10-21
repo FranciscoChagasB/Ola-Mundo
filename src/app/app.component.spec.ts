1885c853ba2c7c28702c498cd74d622ec89a7198
Francisco das Chagas <francisco.chagas05@aluno.ifce.edu.br>
2023-10-21 20:42:30 -0300

    Committing changes in the submodule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Ola-Mundo'`, () => {
+  it(`should have as title 'Olá Mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ola-Mundo');
+    expect(app.title).toEqual('Olá Mundo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Ola-Mundo app is running!');
+    expect(compiled.querySelector('head')?.textContent).toContain('Olá Mundo');
   });
 });
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Ola-Mundo';
+  title = 'Olá Mundo';
 }
